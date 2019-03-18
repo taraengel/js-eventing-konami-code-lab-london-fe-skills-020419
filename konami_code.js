c10b39d5781d8cbff6185a2a352f90548c52e6e2
Tara-Gabriella Engelhardt <taraengy@gmail.com>
2019-03-18 20:54:05 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
   // Write your JavaScript code inside the init() function
  
-
 document.body.addEventListener('keydown', e => onKeyDownHandler(e))
  
 }
